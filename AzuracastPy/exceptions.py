f88c3878f2a8b8ee7090a19307cc9778f91db7b4
AbahGabriel <gabeyiscool43@gmail.com>
2024-01-05 04:03:22 +0100

    New Exception just dropped (UnexpectedErrorException)

--- AzuracastPy/exceptions.py
+++ AzuracastPy/exceptions.py
@@ -1,11 +1,14 @@
 class AzuracastException(Exception):
     """The base Azuracast Exception class that all other exceptions will extend"""
 
+class AzuracastAPIException(AzuracastException):
+    """The base Azuracast Exception class that all other exceptions will extend"""
+
 class AccessDeniedException(AzuracastException):
     """Indicates unauthorized access to certain endpoints."""
 
-class AzuracastAPIException(AzuracastException):
-    """Indicates errors encountered from the Azuracast API."""
+class UnexpectedErrorException(AzuracastException):
+    """Indicates unexpected errors encountered from the Azuracast API."""
 
 class ClientException(AzuracastException):
     """Indicates errors made by the user of the API."""
