f82d420858526c3d9dd2f77dde5ab784c4d111fd
AbahGabriel <gabeyiscool43@gmail.com>
2024-01-05 04:07:30 +0100

    Changed layout of _send_request function

--- AzuracastPy/request_handler.py
+++ AzuracastPy/request_handler.py
@@ -1,7 +1,10 @@
 import requests
-from requests.exceptions import JSONDecodeError
+from requests import exceptions
+from json import decoder
 
-from exceptions import ClientException, AccessDeniedException, AzuracastException
+from .exceptions import (
+    AccessDeniedException, AzuracastAPIException, UnexpectedErrorException, ClientException
+)
 
 from lxml import html # A HTML parser is needed to extract some errors
 
@@ -25,25 +28,22 @@ class RequestHandler:
     def delete(self, url: str):
         return self._send_request(method='DELETE', url=url)
     
+    # -----------------------------------
+    # When testing the API, I ran into multiple instances of NotLoggedIn errors returning a code of 200, as well as
+    # some errors returning a code of 500. In addition to this, some errors returned HTML instead of JSON.
+    # This behaviour seems to be random. As a result, on top of the normal error logic, I added logic to check for these occurences,
+    # just incase. Better safe than sorry I guess.
+    # -----------------------------------
     def _send_request(self, method: str, url: str, body: Optional[str] = None) -> Dict[str, Any]:
-        # When testing the API, I ran into multiple instances of NotLoggedIn errors returning a code of 200, as well as
-        # some errors returning a code of 500. In addition to this, some errors returned HTML instead of JSON.
-        # This behaviour seems to be random. As a result, on top of the normal error logic, I added logic to check for these occurences,
-        # just incase. Better safe than sorry I guess.
         with requests.request(method=method, url=url, data=body, headers=self._headers) as response:
             if response.status_code == 500:
-                try:
-                    error = response.json()
-                    self._raise_request_exception(error['type'], error['message'])
-                except (ValueError, JSONDecodeError):
-                    error = self._get_specific_error(response.text)
-                    self._raise_request_exception(error[0], error[1])
+                self._handle_500_error(url=url, response=response)
 
             if response.status_code == 404:
                 raise ClientException("Requested resource not found.")
             
             if response.status_code == 405:
-                raise AzuracastException(f"The '{method}' method is not allowed on this endpoint ({url}).")
+                raise AzuracastAPIException(f"The '{method}' method is not allowed on this endpoint ({url}).")
             
             if response.status_code == 403:
                 self._raise_access_denied_exception()
@@ -51,7 +51,7 @@ class RequestHandler:
             if response.status_code == 200:
                 try:
                     return response.json()
-                except (ValueError, JSONDecodeError):
+                except (ValueError, KeyError, exceptions.JSONDecodeError, decoder.JSONDecodeError):
                     if self._confirm_login_error(response.text) is True:
                         self._raise_access_denied_exception()
                     
@@ -63,6 +63,21 @@ class RequestHandler:
     def _set_headers(self) -> Dict[str, str]:
         return {'accept': 'application/json', 'X-API-Key': self._x_api_key}
     
+    def _handle_500_error(self, url: str, response: requests.Response):
+        try:
+            # Attempts to parse error json response for details.
+            error = response.json()
+            self._raise_request_exception(error['type'], error['message'])
+        except (ValueError, KeyError, exceptions.JSONDecodeError, decoder.JSONDecodeError):
+            # Error is not valid JSON.
+            # Assumes response is error HTML response and tries to parse it to get details.
+            try:
+                error = self._get_specific_error(response.text)
+                self._raise_request_exception(error[0], error[1])
+            except:
+                # Error is neither JSON nor expected HTML. Unexpected error found.
+                self._raise_unexpected_error_exception(url, response.text)
+
     def _get_specific_error(self, text: str) -> Tuple:
         doc = html.fromstring(text)
         error_title = doc.xpath(".//p[@class='text-muted card-text']")[0].text
@@ -73,16 +88,19 @@ class RequestHandler:
         return (error_title, error_description)
 
     def _confirm_login_error(self, text: str) -> bool:
-        doc = html.fromstring(text)
-        title = doc.find('.//title')
+        try:
+            doc = html.fromstring(text)
+            title = doc.find('.//title')
 
-        return title.text.strip() == 'Log In - AzuraCast'
+            return title.text.strip() == 'Log In - AzuraCast'
+        except:
+            return False
     
-    def _raise_request_exception(self, error_type, error_message):
-        raise AzuracastException(f"Encountered request error: {error_type} - {error_message}")
+    def _raise_request_exception(self, error_type: str, error_message: str):
+        raise AzuracastAPIException(f"Encountered request error: {error_type} - {error_message}")
     
     def _raise_access_denied_exception(self):
         raise AccessDeniedException("You silly goose. You need a valid x-api-key to perform this action. Update yours and try again.")
     
-    def _raise_unexpected_error_exception(self, url, text):
-        raise AzuracastException(f"Unexpected error occured while trying to access this url: {url}.\nError details: {text}")
+    def _raise_unexpected_error_exception(self, url: str, text: str):
+        raise UnexpectedErrorException(f"Unexpected error occured while trying to access this url: {url}.\nError details: {text}")
