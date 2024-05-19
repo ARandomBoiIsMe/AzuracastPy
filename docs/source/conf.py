48530dc44354dda95e1074935079b94ffdb3dec7
ARandomBoiIsMe <gabeyiscool43@gmail.com>
2024-05-19 16:31:52 +0100

    Dark mode for documentation.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,8 +29,10 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_dark_mode'
 html_static_path = ['_static']
+
+default_dark_mode = True
 
 
 def skip(app, what, name, obj, would_skip, options):
